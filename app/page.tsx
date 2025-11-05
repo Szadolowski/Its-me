c7f69c0d4392aeec71d3544e2b9cc16aa5d3fec2
Rafal Curzydlo <rafall.curzydlo@gmail.com>
2025-11-05 15:20:55 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-full font-sans bg-zinc-50 dark:bg-neutral-600">
+      <p className="text-5xl text-red-600">test</p>
+    </div>
+  );
+}
